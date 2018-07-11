50eb23fcc2f0a0a2d22136b6910feda7b0f639e2
CristianRetea <retea.cristian96@gmail.com>
2018-07-11 20:37:56 +0300

    Dashboard CardView+RecyclerView clickable pentru UserJourneyList si Chat

---
+++ app/src/main/java/ro/changeneers/apprentice/DashboardItem.java
@@ -0,0 +1,44 @@
+package ro.changeneers.apprentice;
+
+/**
+ * Created by retea on 11-Jul-18.
+ */
+
+public class DashboardItem {
+    private String Title;
+    private String Description;
+    private int Thumbnail;
+
+    public DashboardItem() {
+    }
+
+    public DashboardItem(String title, String description, int thumbnail) {
+        Title = title;
+        Description = description;
+        Thumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getThumbnail() {
+        return Thumbnail;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        Thumbnail = thumbnail;
+    }
+}
