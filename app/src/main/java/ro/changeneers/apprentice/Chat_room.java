f662f95bce2f5df7c38de3ab001fe927a1038202
Andrei <andreyminea@gmail.com>
2018-07-17 15:57:08 +0300

    Chat

--- app/src/main/java/ro/changeneers/apprentice/Chat_room.java
+++ app/src/main/java/ro/changeneers/apprentice/Chat_room.java
@@ -1,67 +1,110 @@
 package ro.changeneers.apprentice;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-public class Chat_room extends AppCompatActivity
-{
-    Button sendBtn;
-    TextView received;
-    EditText sendMsg;
 
-    DatabaseReference rootRoomName;
+public class Chat_Room  extends AppCompatActivity{
 
-    String roomName;
-    String userName;
+    private Button btn_send_msg;
+    private EditText input_msg;
+    private TextView chat_conversation;
 
+    private String user_name,room_name;
+    private DatabaseReference root ;
+    private String temp_key;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
+        setContentView(R.layout.chat_room);
 
-        sendBtn = (Button) findViewById(R.id.sendBtn);
-        received = (TextView) findViewById(R.id.received);
-        sendMsg = (EditText) findViewById(R.id.sendMsg);
+        btn_send_msg = (Button) findViewById(R.id.sendBtn);
+        input_msg = (EditText) findViewById(R.id.sendMsg);
+        chat_conversation = (TextView) findViewById(R.id.textView);
 
-        //Get room name and user name
+        user_name = getIntent().getExtras().get("user_name").toString();
+        room_name = getIntent().getExtras().get("room_name").toString();
+        setTitle(" Room - "+room_name);
 
-        roomName = getIntent().getExtras().getString("Room_name");
+        root = FirebaseDatabase.getInstance().getReference().child(room_name);
 
-        userName = getIntent().getExtras().getString("User_name");
-
-
-        setTitle(roomName);
-
-        rootRoomName = FirebaseDatabase.getInstance().getReference().getRoot().child(roomName);
-
-        sendBtn.setOnClickListener(new View.OnClickListener() {
+        btn_send_msg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                DatabaseReference childroot = rootRoomName.push();
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("name", userName);
-                map.put("message", sendMsg.getText().toString());
-                childroot.updateChildren(map);
+            public void onClick(View view) {
+
+                Map<String,Object> map = new HashMap<String, Object>();
+                temp_key = root.push().getKey();
+                root.updateChildren(map);
+
+                DatabaseReference message_root = root.child(temp_key);
+                Map<String,Object> map2 = new HashMap<String, Object>();
+                map2.put("name",user_name);
+                map2.put("msg",input_msg.getText().toString());
+
+                message_root.updateChildren(map2);
             }
         });
 
+        root.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
+                append_chat_conversation(dataSnapshot);
+            }
 
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
+                append_chat_conversation(dataSnapshot);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private String chat_msg,chat_user_name;
+
+    private void append_chat_conversation(DataSnapshot dataSnapshot) {
+
+        Iterator i = dataSnapshot.getChildren().iterator();
+
+        while (i.hasNext()){
+
+            chat_msg = (String) ((DataSnapshot)i.next()).getValue();
+            chat_user_name = (String) ((DataSnapshot)i.next()).getValue();
+
+            chat_conversation.append(chat_user_name +" : "+chat_msg +" \n");
+        }
 
 
     }
