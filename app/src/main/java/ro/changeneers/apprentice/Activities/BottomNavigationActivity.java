51d7a5c956250d3af2683ca773aa7952e6731b5f
Cristian Retea-laptop <retea.cristian96@gmail.com>
2018-07-18 12:46:11 +0300

    viewpager + bottomnavigation working

--- app/src/main/java/ro/changeneers/apprentice/Activities/BottomNavigationActivity.java
+++ app/src/main/java/ro/changeneers/apprentice/Activities/BottomNavigationActivity.java
@@ -5,8 +5,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.MenuItem;
+import android.view.View;
 
+import ro.changeneers.apprentice.Adapters.ViewPagerAdapter;
 import ro.changeneers.apprentice.Fragments.FragmentBani;
 import ro.changeneers.apprentice.Fragments.FragmentJobs;
 import ro.changeneers.apprentice.Fragments.FragmentOverview;
@@ -16,34 +19,71 @@ import ro.changeneers.apprentice.R;
 
 public class BottomNavigationActivity extends NavDrawer {
 
+    private ViewPagerAdapter mViewPagerAdapter;
+    private ViewPager mViewPager;
+    private MenuItem prevMenuItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        mViewPager = (ViewPager) findViewById(R.id.fragment_container);
+        final BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
 
         Intent receivingIntent = getIntent();
         int position = receivingIntent.getIntExtra("position",0);
-        switch(position){
-            case 0:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentOverview()).commit();
-                bottomNav.setSelectedItemId(R.id.nav_info);
-                break;
-            case 1:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentBani()).commit();
-                bottomNav.setSelectedItemId(R.id.nav_bani);
-                break;
-            case 2:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentStres()).commit();
-                bottomNav.setSelectedItemId(R.id.nav_stres);
-                break;
-            case 3:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentJobs()).commit();
-                bottomNav.setSelectedItemId(R.id.nav_jobs);
-                break;
-        }
+        setupViewPager(mViewPager,position,bottomNav);
+
+
+//        switch(position){
+//            case 0:
+////                mViewPager.setCurrentItem(0);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentOverview()).commitAllowingStateLoss();
+//                bottomNav.setSelectedItemId(R.id.nav_info);
+//                break;
+//            case 1:
+////                mViewPager.setCurrentItem(1);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentBani()).commitAllowingStateLoss();
+//                bottomNav.setSelectedItemId(R.id.nav_bani);
+//                break;
+//            case 2:
+////                mViewPager.setCurrentItem(2);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentStres()).commitAllowingStateLoss();
+//                bottomNav.setSelectedItemId(R.id.nav_stres);
+//                break;
+//            case 3:
+//                mViewPager.setCurrentItem(3);
+////                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentJobs()).commitAllowingStateLoss();
+//                bottomNav.setSelectedItemId(R.id.nav_jobs);
+//                break;
+//        }
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNav.getMenu().getItem(0).setChecked(false);
+                }
+                bottomNav.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNav.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 
@@ -55,24 +95,54 @@ public class BottomNavigationActivity extends NavDrawer {
 
                     switch(item.getItemId()){
                         case R.id.nav_info:
-                            selectedFragment = new FragmentOverview();
+                            mViewPager.setCurrentItem(0);
                             break;
                         case R.id.nav_bani:
-                            selectedFragment = new FragmentBani();
+                            mViewPager.setCurrentItem(1);
                             break;
                         case R.id.nav_stres:
-                            selectedFragment = new FragmentStres();
+                            mViewPager.setCurrentItem(2);
                             break;
                         case R.id.nav_jobs:
-                            selectedFragment = new FragmentJobs();
+                            mViewPager.setCurrentItem(3);
                             break;
                         case R.id.nav_start:
-                            selectedFragment = new FragmentStartJourney();
+                            mViewPager.setCurrentItem(4);
                             break;
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                            selectedFragment).commitAllowingStateLoss();
                     return true;
                 }
             };
+
+    private void setupViewPager(ViewPager viewpager,int position,BottomNavigationView bnv){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        FragmentOverview fov = new FragmentOverview();
+        FragmentBani fb = new FragmentBani();
+        FragmentStres fs = new FragmentStres();
+        FragmentJobs fj = new FragmentJobs();
+        FragmentStartJourney fsj = new FragmentStartJourney();
+        adapter.addFragment(fov);
+        adapter.addFragment(fb);
+        adapter.addFragment(fs);
+        adapter.addFragment(fj);
+        adapter.addFragment(fsj);
+        mViewPager.setAdapter(adapter);
+        viewpager.setCurrentItem(position);
+        switch (position){
+            case 0:
+                bnv.setSelectedItemId(R.id.nav_info);
+                break;
+            case 1:
+                bnv.setSelectedItemId(R.id.nav_bani);
+                break;
+            case 2:
+                bnv.setSelectedItemId(R.id.nav_stres);
+                break;
+            case 3:
+                bnv.setSelectedItemId(R.id.nav_jobs);
+                break;
+        }
+    }
 }
