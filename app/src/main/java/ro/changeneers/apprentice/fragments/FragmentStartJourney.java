d875289d31219501093a9967d26baa1872e88857
simona-chirila <simochi96@gmail.com>
2018-08-14 18:37:39 +0300

    buton start 2

--- app/src/main/java/ro/changeneers/apprentice/fragments/FragmentStartJourney.java
+++ app/src/main/java/ro/changeneers/apprentice/fragments/FragmentStartJourney.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ro.changeneers.apprentice.R;
 import ro.changeneers.apprentice.activities.JourneyActivity;
@@ -22,30 +24,37 @@ public class FragmentStartJourney extends android.support.v4.app.Fragment {
     private Button startJourneyBtn;
     private TextView titlu;
     private TextView descriere;
-    private TextView mesaj;
+    //private TextView mesaj;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_start_journey_layout,container,false);
 
         startJourneyBtn = view.findViewById(R.id.ButtonStartJourney);
         titlu = view.findViewById(R.id.titlu_start);
         descriere = view.findViewById(R.id.desc_start);
-        mesaj = view.findViewById(R.id.mesaj_start);
+        //mesaj = view.findViewById(R.id.mesaj_start);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             descriere.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
         }
 
         startJourneyBtn.setOnClickListener(new View.OnClickListener() {
+
+            //boolean visible;
+
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getActivity(),JourneyActivity.class);
-                startActivity(intent);
-                getActivity().finish();
+                /*TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                mesaj.setVisibility(visible ? View.VISIBLE : View.GONE);*/
 
+                        Intent intent = new Intent(getActivity(),JourneyActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                Toast.makeText(getActivity(), "MULTĂ BAFTĂ !!!", Toast.LENGTH_LONG).show();
 
             }
         });
